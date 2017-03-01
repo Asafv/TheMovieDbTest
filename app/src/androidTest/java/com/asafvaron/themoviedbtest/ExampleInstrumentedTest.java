32b666237f601b11ef5cf8dcfd1f14bc2a704f44
asafvaron <asafv@tikalk.com>
2017-03-01 14:49:31 +0200

    add some unit tests

--- app/src/androidTest/java/com/asafvaron/themoviedbtest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/asafvaron/themoviedbtest/ExampleInstrumentedTest.java
@@ -1,13 +1,22 @@
 package com.asafvaron.themoviedbtest;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.asafvaron.themoviedbtest.activity.MainActivity;
+import com.asafvaron.themoviedbtest.model.Movie;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -15,12 +24,46 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.asafvaron.themoviedbtest", appContext.getPackageName());
+    private Movie movie = new Movie("https://image.tmdb.org/t/p/w500" + "/wnVHDbGz7RvDAHFAsVVT88FxhrP.jpg",
+            false, "sheker sheker sheker sheker sheker sheker",
+            "2017-02-08", null, 341174, "", "", "Fifty Shades Darker", "", 6.123, 123, false, 6.9);
+
+    private MainActivity mMainActivity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void init() {
+        mMainActivity = mActivityRule.getActivity();
+    }
+
+    @Test
+    public void gridMovies_DisplayedUi() throws Exception {
+        // check if grid fragment was loaded with default
+        onView(withText(R.string.action_now_playing)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void infoMovie_InfoDisplayed() {
+
+        // click on one of the items and make sure info fragment is displayed
+//        onView(withId(R.id.rv_grid_list)).perform(click());
+
+        mMainActivity.showInfoFragment(movie);
+
+        // check action bar title has changed
+        onView(withText(R.string.movie_info_ab_title)).check(matches(isDisplayed()));
+
+        // check movie data is displayed
+        onView(withId(R.id.tv_release_date)).check(matches(isDisplayed()));
+        onView(withId(R.id.iv_poster)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_overview)).check(matches(isDisplayed()));
+
     }
 }
--- app/src/test/java/com/asafvaron/themoviedbtest/ExampleUnitTest.java
+++ app/src/test/java/com/asafvaron/themoviedbtest/ExampleUnitTest.java
@@ -1,8 +1,24 @@
 package com.asafvaron.themoviedbtest;
 
-import org.junit.Test;
+import com.asafvaron.themoviedbtest.adapters.MoviesGridAdapter;
+import com.asafvaron.themoviedbtest.fragments.MovieInfoFragment;
+import com.asafvaron.themoviedbtest.fragments.MoviesGridFragment;
+import com.asafvaron.themoviedbtest.model.Movie;
+import com.asafvaron.themoviedbtest.rest.ApiClient;
+import com.asafvaron.themoviedbtest.rest.ApiInterface;
+import com.google.common.collect.Lists;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +26,53 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    public ExampleUnitTest() {
+        super();
+    }
+
+    @Captor
+    private ArgumentCaptor<MoviesGridAdapter.MoviesGridAdapterListener> moviesGridAdapterListenerArgumentCaptor;
+
+    @Mock
+    private static List<Movie> MOVIE_LIST = Lists.newArrayList(
+            new Movie("https://image.tmdb.org/t/p/w500" + "/wnVHDbGz7RvDAHFAsVVT88FxhrP.jpg",
+                    false, "sheker sheker sheker sheker sheker sheker",
+                    "2017-02-08", null, 341174, "", "", "Fifty Shades Darker", "", 6.123, 123, false, 6.9),
+            new Movie("https://image.tmdb.org/t/p/w500" + "/bbxtz5V0vvnTDA2qWbiiRC77Ok9.jpg",
+                    false, "sheker sheker sheker sheker sheker sheker",
+                    "2017-02-08", null, 14564, "", "", "Rings", "", 6.123, 123, false, 6.9)
+    );
+
+    @Mock
+    private static List<Movie> EMPTY_MOVIE_LIST = new ArrayList<>(0);
+
+    private ApiInterface mApiInterface;
+
+    @Mock
+    private MovieInfoFragment mMovieInfoFragment;
+
+    @Mock
+    private MoviesGridFragment mMoviesGridFragment;
+
+    @Before
+    public void setupViews() {
+        // Mockito has a very convenient way to inject mocks by using the @Mock annotation. To
+        // inject the mocks in the test the initMocks method needs to be called.
+        MockitoAnnotations.initMocks(this);
+
+        mMoviesGridFragment = new MoviesGridFragment();
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+    }
+
+    @Test
+    public void MovieGrid_loadMovies() {
+//        verify(mApiInterface).getNowPlayingMovies(ApiClient.API_KEY);
+//        verify(mApiInterface).getTopRatedMovies(ApiClient.API_KEY);
+//        verify(mApiInterface).getPopularMovies(ApiClient.API_KEY);
+//        verify(mApiInterface).getUpcomingMovies(ApiClient.API_KEY);
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
