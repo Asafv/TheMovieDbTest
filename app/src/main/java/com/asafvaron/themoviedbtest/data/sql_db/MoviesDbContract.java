c40af90bc269a21b9af4aa8cd3864ab666574caf
Asaf Varon <asafvaron@Asafs-MacBook-Pro.local>
2017-05-07 17:18:34 +0300

    coding

--- app/src/main/java/com/asafvaron/themoviedbtest/data/sql_db/MoviesDbContract.java
+++ app/src/main/java/com/asafvaron/themoviedbtest/data/sql_db/MoviesDbContract.java
@@ -36,6 +36,26 @@ public class MoviesDbContract {
         public static final String COLUMN_TYPE = "type";
     }
 
+    public static class Favorites implements BaseColumns {
+        // table name
+        public static final String TABLE_NAME = "favorites_table";
+
+        //uri
+        public final static Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE_NAME);
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_ORIGINAL_TITLE = "original_title";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_POSTER = "poster";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_VOTE_COUNT = "vote_count";
+        public static final String COLUMN_RUNTIME = "runtime";
+        public static final String COLUMN_TYPE = "type";
+    }
+
+
     public class MovieTypes {
         public static final String NOW_PLAYING = "Now Playing";
         public static final String POPULAR = "Popular";
